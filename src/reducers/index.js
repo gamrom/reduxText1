ba8c7c387a05e54f424355f4178ff000607b5098
gamrom <gamrom01@naver.com>
2021-12-07 13:19:48 +0900

    commit 1

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+const Index = () => {
+	return <div>하이</div>;
+};
+
+export default Index;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title: "Javascript: Teh Good Parts" },
+		{ title: "HarryPotter" },
+		{ title: "Haha" },
+		{ title: "Your great" },
+	];
+}
